c22685ce4332ffef1a1430481a63e2221f8d5a73
Carlos Souza <carloshrsouza@gmail.com>
2016-07-07 11:33:54 -0400

    Print name with heavy processing

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	names := []string{"Orlando", "Miami", "Tampa", "Jacksonville"}
@@ -10,5 +13,9 @@ func main() {
 }
 
 func printName(n string) {
+	result := 0.0
+	for i := 0; i < 100000000; i++ {
+		result += math.Pi * math.Sin(float64(len(n)))
+	}
 	fmt.Println(n)
 }
