ca7ec9a281d209b12828a38a5e5b9064853fb902
Carlos Souza <carloshrsouza@gmail.com>
2016-07-07 11:48:43 -0400

    Run concurrently

--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"fmt"
 	"math"
+	"sync"
 )
 
 func main() {
 	names := []string{"Orlando", "Miami", "Tampa", "Jacksonville"}
+
+	var wg sync.WaitGroup
+	wg.Add(len(names))
+
 	for _, name := range names {
-		printName(name)
+		go printName(name, &wg)
 	}
+
+	wg.Wait()
 }
 
-func printName(n string) {
+func printName(n string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	result := 0.0
 	for i := 0; i < 100000000; i++ {
 		result += math.Pi * math.Sin(float64(len(n)))
